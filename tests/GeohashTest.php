9fe33c35c69ec17ca74514b8747eafa8b1883cd5
Saikiran Ch <saikiranchavan@gmail.com>
2017-12-08 22:11:17 +0530

    Added sample php unit tests

---
+++ tests/GeohashTest.php
@@ -0,0 +1,18 @@
+<?php
+ 
+use ImSaikiran\Geohash\Geohash;
+ 
+class GeohashTest extends PHPUnit_Framework_TestCase {
+ 
+    public function testEncode()
+    {
+        $geohash = new Geohash;
+        $this->assertTrue(true);
+    }
+
+    public function testDecode() 
+    {
+        $geohash = new Geohash;
+        $this->assertTrue(true);
+    }
+}
